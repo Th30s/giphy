5071ddb21b65fa25dc557ec204a825b9c01d0a7c
Th30s <yahir.garcia@uasdc.org>
2020-03-30 21:38:53 +0000

    3/30/2020: Completed API call, image render, and image randomizer

--- js/script.js
+++ js/script.js
@@ -4,7 +4,17 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
+let rng = Math.floor(Math.random()*100)   
+console.log(rng) 
+let search = $('#search-term').val();  
+    fetch('https://api.giphy.com/v1/gifs/search?q=' + search + '&rating=pg&api_key=dc6zaTOxFJmzC')
+  .then(function(response){
+      return response.json();
+  })
+  .then(function(data) {
+    console.log(data.data[0].images.original.url);
+    $('#img').html('<img src=' + data.data[rng].images.original.url + '>')
+  }) 
   
   
 });
